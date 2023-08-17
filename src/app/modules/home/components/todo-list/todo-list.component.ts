b42a7613db92d37fea25a6ce9fec383cf847e3ec
Arthur Correia <arthurcorria0109@gmail.com>
2023-08-17 18:34:56 -0300

    finalizado
    finalizei o projeto

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Task } from '../../model/task';
 
 @Component({
@@ -10,10 +10,34 @@ export class TodoListComponent {
   public taskList: Task[];
 
   constructor () { 
-    this.taskList = [];
+    this.taskList = JSON.parse(localStorage['taskList'] || '[]');
   }
 
   setNewTask(event: Task): void { this.taskList.push(event); }
+
+  ngDoCheck(): void {
+    this.updateLocalStorage();
+    this.sortTaskList();
+  }
+
+  updateLocalStorage(): string | void {
+    if (this.taskList) {
+      return localStorage.setItem('taskList', JSON.stringify(this.taskList));
+    }
+  }
+
+  sortTaskList(): Task[] {
+    return this.taskList.sort((first, last) => Number(first.status) - Number(last.status));
+  }
+
+  validationInput(name: string, i: number): void {
+    if (!name.length) {
+      const confirm = window.confirm('Tarefa vazia, deseja deletar?');
+      if(confirm) {
+        this.deleteTask(i);
+      }
+    }
+  }
 
   deleteTask(i: number): void { this.taskList.splice(i, 1); }
 
